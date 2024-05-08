2e6592c17fac3572c7c4926579e4acfb38842f46
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-05-08 15:16:09 +0200

    done

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const nameString = s1 + s2 + s3 + s4 + s5
 
 // Print out the concatenated string
-
+console.log(nameString)
 
 
 
@@ -22,12 +22,14 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const capPart1 = part1.slice(0,3) + part1.charAt(3).toUpperCase()
+const capPart2 = part2.slice(0,5) + part2.charAt(5).toUpperCase()
+const concParts = capPart1 + capPart2
 
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(capPart1);
+console.log(capPart2);
+console.log(concParts);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -35,10 +37,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipAmount = billTotal * 0.15
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,10 +49,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randNum = Math.random() * 10
 
 // Print the generated random number
-
+console.log(randNum)
 
 
 /*******************************************
@@ -62,15 +64,32 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
 
 const expression2 = a || b;
+// true
 
 const expression3 = !a && b;
+// true --> WRONG: should be false
 
 const expression4 = !(a && b);
+// true
 
 const expression5 = !a || !b;
+// true
 
 const expression6 = !(a || b);
+// false
 
 const expression7 = a && a;
+// true
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
+
+
